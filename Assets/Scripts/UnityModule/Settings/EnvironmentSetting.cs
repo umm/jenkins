406faa0e837849f5c98e6e4319219e319afb5175
fakestarbaby <fakestarbaby@gmail.com>
2017-10-03 21:41:56 +0900

    :sparkles: Improve environment setting
    Jenkins で利用する項目は、EnvironmentSetting の Path ではなく Jenkins に定義する

--- Assets/Scripts/UnityModule/Settings/EnvironmentSetting.cs
+++ Assets/Scripts/UnityModule/Settings/EnvironmentSetting.cs
@@ -1,82 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace UnityModule.Settings {
     public partial class EnvironmentSetting {
-        public partial class EnvironmentSetting_Path {
-            /// <summary>
-            /// 初期値: Slack ユーザ名称
-            /// </summary>
-            private const string DEFAULT_SLACK_USER_NAME = "";
+        [Serializable]
+        public struct EnvironmentSetting_Jenkins {
+            public string SlackUserName;
+            public string UserId;
+            public string Password;
+            public string JobName;
+        }
 
-            /// <summary>
-            /// 初期値: Jenkins ユーザ ID
-            /// </summary>
-            private const string DEFAULT_JENKINS_USER_ID = "";
-
-            /// <summary>
-            /// 初期値: Jenkins パスワード
-            /// </summary>
-            private const string DEFAULT_JENKINS_PASSWORD = "";
-
-            /// <summary>
-            /// 初期値: Jenkins ジョブ名称
-            /// </summary>
-            private const string DEFAULT_JENKINS_JOB_NAME = "";
-
-            /// <summary>
-            /// Slack ユーザ名称
-            /// </summary>
-            [SerializeField]
-            private string slackUserName = DEFAULT_SLACK_USER_NAME;
-            public string SlackUserName {
-                get {
-                    return this.slackUserName;
-                }
-                set {
-                    this.slackUserName = value;
-                }
+        [SerializeField]
+        private EnvironmentSetting_Jenkins jenkins;
+        public EnvironmentSetting_Jenkins Jenkins {
+            get {
+                return this.jenkins;
             }
-
-            /// <summary>
-            /// Jenkins ユーザ ID
-            /// </summary>
-            [SerializeField]
-            private string jenkinsUserId = DEFAULT_JENKINS_USER_ID;
-            public string JenkinsUserId {
-                get {
-                    return this.jenkinsUserId;
-                }
-                set {
-                    this.jenkinsUserId = value;
-                }
-            }
-
-            /// <summary>
-            /// Jenkins パスワード
-            /// </summary>
-            [SerializeField]
-            private string jenkinsPassword = DEFAULT_JENKINS_PASSWORD;
-            public string JenkinsPassword {
-                get {
-                    return this.jenkinsPassword;
-                }
-                set {
-                    this.jenkinsPassword = value;
-                }
-            }
-
-            /// <summary>
-            /// Jenkins ジョブ名称
-            /// </summary>
-            [SerializeField]
-            private string jenkinsJobName = DEFAULT_JENKINS_JOB_NAME;
-            public string JenkinsJobName {
-                get {
-                    return this.jenkinsJobName;
-                }
-                set {
-                    this.jenkinsJobName = value;
-                }
+            set {
+                this.jenkins = value;
             }
         }
     }
