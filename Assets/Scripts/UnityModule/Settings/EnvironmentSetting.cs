ae74cfd1f25b7e91628e0d4732f09b16dbf3f9eb
Tetsuya Mori <monry84@gmail.com>
2017-10-05 13:13:04 +0900

    :sparkles::clipboard: Add BaseURL field and split JobName
    * URL はベタ書きにせずユーザに設定させる
    * ジョブ名を Player 用と AssetBundle 用のそれぞれを設定可能にする

--- Assets/Scripts/UnityModule/Settings/EnvironmentSetting.cs
+++ Assets/Scripts/UnityModule/Settings/EnvironmentSetting.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 
 namespace UnityModule.Settings {
     public partial class EnvironmentSetting {
+
         [Serializable]
         public struct EnvironmentSetting_Jenkins {
             public string SlackUserName;
             public string UserId;
             public string Password;
-            public string JobName;
+            [Tooltip("ジョブ名の手前までの URL を設定してください")]
+            public string BaseURL;
+            public string JobNameForPlayer;
+            public string JobNameForAssetBunde;
+
         }
 
         [SerializeField]
